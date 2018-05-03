bed400ff2b1d637f3452ed93e761362589ba9e11
Peter Siemer <affenrakete@users.noreply.github.com>
2018-05-03 09:43:30 +0200

    Update clear_tc_cache.bat
    GPL Head

--- clear_tc_cache.bat
+++ clear_tc_cache.bat
@@ -1,3 +1,19 @@
+:: clear teamcenter cache
+:: Copyright (C) 2018  Peter Siemer
+:: 
+:: This program is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+::
+:: This program is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU General Public License for more details.
+::
+:: You should have received a copy of the GNU General Public License
+:: along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 @echo off
 
 CLS
@@ -63,7 +79,7 @@ echo.
 IF %u% EQU 1 GOTO :Ende
 
 echo Warten um Prozesse sicher zu beenden...
-timeout /T 5
+timeout /T 5 /NOBREAK
 echo.
 echo Cache leeren (Normal)
 echo.
